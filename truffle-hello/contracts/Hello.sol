f31dfee997549aba8f7555e37525c168c1966278
Diego Pacheco <diego.pacheco.it@gmail.com>
2017-07-23 20:39:34 -0300

    Added deposit

--- truffle-hello/contracts/Hello.sol
+++ truffle-hello/contracts/Hello.sol
@@ -1,9 +1,14 @@
-pragma solidity ^0.4.2;
+  pragma solidity ^0.4.2;
 
 contract Hello{
     uint public balance;
 
     function Hello(){
         balance = 1000;
+    }
+
+    function deposit(uint _value) returns (uint _newValue){
+        balance += _value;
+        return balance;
     }
 }
