24a6de646c54c9cbb9c72331e462618966baa1ed
Diego Pacheco <diego.pacheco.it@gmail.com>
2017-07-23 19:44:24 -0300

    Migrate and deploy first contract

--- truffle-hello/migrations/2_deploy_contracts.js
+++ truffle-hello/migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+//var ConvertLib = artifacts.require("./ConvertLib.sol");
+//var MetaCoin = artifacts.require("./MetaCoin.sol");
+var Hello = artifacts.require("./Hello.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  //deployer.deploy(ConvertLib);
+  //deployer.link(ConvertLib, MetaCoin);
+  //deployer.deploy(MetaCoin);
+  deployer.deploy(Hello);
 };
