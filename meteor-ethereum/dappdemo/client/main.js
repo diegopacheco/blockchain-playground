fed31f2f3fec79dff4d549cab8d54db51bf733de
Diego Pacheco <diego.pacheco.it@gmail.com>
2017-07-23 23:21:52 -0300

    added meteor + web3 + ethereum

---
+++ meteor-ethereum/dappdemo/client/main.js
@@ -0,0 +1,26 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  this.counter = new ReactiveVar(0);
+});
+
+Template.hello.helpers({
+  counter() {
+    web3.eth.getBalance("0xCF914Ab76504ee978D29710bAC3797A50E264923",
+             function (error,result) {
+                  alert(result);
+             }
+    )
+  },
+});
+
+Template.hello.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+  },
+});
