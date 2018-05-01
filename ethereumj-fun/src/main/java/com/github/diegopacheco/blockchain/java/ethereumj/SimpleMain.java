7a5556af96780268443325920fff9d8b95258664
Diego Pacheco <diego.pacheco.it@gmail.com>
2018-05-01 03:53:37 -0300

    .

--- ethereumj-fun/src/main/java/com/github/diegopacheco/blockchain/java/ethereumj/SimpleMain.java
+++ ethereumj-fun/src/main/java/com/github/diegopacheco/blockchain/java/ethereumj/SimpleMain.java
@@ -56,6 +56,7 @@ public class SimpleMain extends EthereumListenerAdapter {
   }
 	
 	public static void main(String[] args) throws Exception {
+		ethereum.addListener(new SimpleMain());
 	}
 
 	@Override
